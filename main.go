d2acc8bb8b4ecbed989585773703c9f2c89a425f
smallForest <1032817724@qq.com>
2019-01-09 16:13:44 +0800

    格式化一下main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	jssdk := jssdk.New("wx02d6c9061******", "7acfb40fb2f70cd331a*******","http://a.com")
+	jssdk := jssdk.New("wx02d6c9061******", "7acfb40fb2f70cd331a*******", "http://a.com")
 	config := jssdk.GetWechatConfig()
 	fmt.Println(config)
 }
